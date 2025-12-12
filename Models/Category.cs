1f780c3f0a36a7fcc3fdf21cf91f424d9b024093
hodoantrang12 <doantranghothi2005@gmail.com>
2025-12-12 12:00:59 +0700

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EyelixEyewear.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
